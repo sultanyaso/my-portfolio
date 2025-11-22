d31ac0ced33d2ada42957ec4063be1696913ed33
Yasir Sultan <sultanyasir990@gmail.com>
2025-11-23 00:38:40 +0500

    Update the index file (Hash router for github pages

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // optional
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// src/index.js (Example using HashRouter for GitHub Pages)
+
+
+// Install React Router first: npm install react-router-dom
+import { HashRouter } from 'react-router-dom';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
