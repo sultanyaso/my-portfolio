6947c47b0efc673361231be4d5aab708ade9cf0b
Yasir Sultan <sultanyasir990@gmail.com>
2025-11-24 00:34:47 +0500

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import WhatsAppButton from "./components/WhatsAppButton";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <Projects />
       <Contact />
+      <WhatsAppButton />  
     </div>
   );
 }
