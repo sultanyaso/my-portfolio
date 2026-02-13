241b96d2f0e8759030ccf31341c3b712010fbf44
Yasir Sultan <sultanyasir990@gmail.com>
2026-02-13 23:52:39 +0500

    Fix CV download path

--- src/components/Header.js
+++ src/components/Header.js
@@ -122,7 +122,7 @@ const Header = () => {
           <li><a href="#projects">PORTFOLIO</a></li>
           <li><a href="#contact">CONTACT</a></li>
         </ul>
-        <a href="/CV_Yasir.pdf" download= "Yasir_Sultan_CV.pdf" className="download-cv">
+        <a href="CV_Yasir.pdf" download= "Yasir_Sultan_CV.pdf" className="download-cv">
           DOWNLOAD CV
         </a>
       </nav>
